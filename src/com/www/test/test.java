a174125284a8f8322ee8e687976df18bf8a43ad2
wz <531970615@qq.com>
2020-02-29 10:34:09 +0800

    first

---
+++ src/com/www/test/test.java
@@ -0,0 +1,11 @@
+package com.www.test;
+
+/**
+ * @author shkstart
+ * @create 2020-02-24 14:49
+ */
+public class test {
+        public static void main(String[] args){
+            System.out.println("Hello world");
+        }
+}
